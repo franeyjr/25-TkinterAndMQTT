cad1e3df35a77c193e051512b73e379f8d2f1850
franeyjr <franeyjr@rose-hulman.edu>
2019-02-04 16:16:58 -0500

    NOT DONE

--- src/m2_tkinter_as_mqtt_sender.py
+++ src/m2_tkinter_as_mqtt_sender.py
@@ -2,7 +2,7 @@
 Using a fake robot as the receiver of messages.
 """
 
-# TODO: 1. In mqtt_remote_method_calls, set LEGO_NUMBER at line 131
+# DONE: 1. In mqtt_remote_method_calls, set LEGO_NUMBER at line 131
 # to YOUR robot's number.
 
 # TODO: 2. Copy your Tkinter/ttk ROBOT gui code from the previous session (m6).
@@ -13,3 +13,82 @@ Using a fake robot as the receiver of messages.
 #   where X and Y are from the entry box.
 #
 # Implement and test.
+import tkinter
+from tkinter import ttk
+import mqtt_remote_method_calls as com
+import time
+
+def main():
+
+    name1 = input("Enter one name (subscriber): ")
+    name2 = input("Enter another name (publisher): ")
+
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect(name1, name2)
+    time.sleep(1)
+
+    root = tkinter.Tk()
+    root.title("MQTT Remote")
+
+    main_frame = ttk.Frame(root, padding=20)
+    main_frame.grid()  # only grid call that does NOT need a row and column
+
+    left_speed_label = ttk.Label(main_frame, text="Left")
+    left_speed_label.grid()
+    left_speed_entry = ttk.Entry(main_frame, width=8)
+    left_speed_entry.insert(0, "600")
+    left_speed_entry.grid()
+
+    right_speed_label = ttk.Label(main_frame, text="Right")
+    right_speed_label.grid()
+    right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
+    right_speed_entry.insert(0, "600")
+    right_speed_entry.grid()
+
+    forward_button = ttk.Button(main_frame, text="Forward")
+    forward_button.grid()
+    forward_button['command'] = lambda: mqtt_client.send_message("forward", [left_speed_entry,right_speed_entry])
+    root.bind('<Up>', lambda event: print("Forward key"))
+
+    left_button = ttk.Button(main_frame, text="Left")
+    left_button.grid()
+    left_button['command'] = lambda: mqtt_client.send_message("turn_left")
+    root.bind('<Left>', lambda event: print("Left key"))
+
+    stop_button = ttk.Button(main_frame, text="Stop")
+    stop_button.grid()
+    stop_button['command'] = lambda: mqtt_client.send_message("stop")
+    root.bind('<space>', lambda event: print("Stop key"))
+
+    right_button = ttk.Button(main_frame, text="Right")
+    right_button.grid()
+    right_button['command'] = lambda: mqtt_client.send_message("turn_right")
+    root.bind('<Right>', lambda event: print("Right key"))
+
+    back_button = ttk.Button(main_frame, text="Back")
+    back_button.grid()
+    back_button['command'] = lambda: mqtt_client.send_message("reverse", [left_speed_entry,right_speed_entry])
+    root.bind('<Down>', lambda event: print("Back key"))
+
+    up_button = ttk.Button(main_frame, text="Up")
+    up_button.grid()
+    up_button['command'] = lambda: mqtt_client.send_message("arm_up")
+    root.bind('<u>', lambda event: print("Up key"))
+
+    down_button = ttk.Button(main_frame, text="Down")
+    down_button.grid()
+    down_button['command'] = lambda: mqtt_client.send_message("arm_down")
+    root.bind('<j>', lambda event: print("Down key"))
+
+    # Buttons for quit and exit
+    q_button = ttk.Button(main_frame, text="Quit")
+    q_button.grid()
+    q_button['command'] = lambda: mqtt_client.send_message("quit")
+
+    e_button = ttk.Button(main_frame, text="Exit")
+    e_button.grid()
+    e_button['command'] = lambda: mqtt_client.send_message("exit_remote")
+
+    root.mainloop()
+
+main()
